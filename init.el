c3cbcf30bfbcdfc11319072df4504b8db23c7c07
Tim Licata <tlicata@hotpads.com>
2010-08-18 03:29:25 -0400

    enable undo-tree by default. try out keybindings.

--- init.el
+++ init.el
@@ -50,7 +50,9 @@
 
 ;; undo-tree
 (require 'undo-tree)
-;(global-undo-tree-mode)
+(global-undo-tree-mode)
+(global-set-key (kbd "C-c u") 'undo-tree-undo)
+(global-set-key (kbd "C-c r") 'undo-tree-redo)
 
 ;; magit.el
 (require 'magit)
