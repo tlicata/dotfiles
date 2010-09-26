21e9365400dd27879fcf579d7ec904907bb3444b
Tim Licata <tlicata@hotpads.com>
2010-09-26 10:38:12 -0400

    open all files marked with * in dired mode by pressing F

--- init.el
+++ init.el
@@ -81,6 +81,17 @@
 (add-to-list 'auto-mode-alist '("\\.js$" . ecmascript-mode))
 (add-to-list 'auto-mode-alist '("\\.json$" . ecmascript-mode))
 
+;; Dired
+;; http://stackoverflow.com/questions/1110118/in-emacs-dired-how-to-find-visit-multiple-files
+(eval-after-load "dired"
+  '(progn
+     (define-key dired-mode-map "F" 'my-dired-find-file)
+     (defun my-dired-find-file (&optional arg)
+       "Open each of the marked files, or the file under the point, or when prefix arg, the next N files "
+       (interactive "P")
+       (let* ((fn-list (dired-get-marked-files nil arg)))
+         (mapc 'find-file fn-list)))))
+
 ;;; This was installed by package-install.el.
 ;;; This provides support for the package system and
 ;;; interfacing with ELPA, the package archive.
