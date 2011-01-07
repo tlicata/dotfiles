f1db9385f4286ae08d50fe2ba4210881c7239f0a
Tim Licata <tlicata@hotpads.com>
2011-01-07 00:13:38 -0500

    trying out org mode

--- init.el
+++ init.el
@@ -57,6 +57,12 @@
 (setq inhibit-splash-screen t)
 (shell)
 
+;; org mode
+(add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
+(global-set-key "\C-cl" 'org-store-link)
+(global-set-key "\C-ca" 'org-agenda)
+(global-set-key "\C-cb" 'org-iswitchb)
+
 ;; auto-complete
 (require 'auto-complete-config)
 (add-to-list 'ac-dictionary-directories "~/.emacs.d/auto-complete/dict")
