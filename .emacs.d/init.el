5609c5a13aba7aead7b579acac45c5aca7a03580
Tim Licata <tim@timothylicata.com>
2014-07-25 11:10:53 -0700

    prevent node prompt from containing weird characters in eshell

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -33,6 +33,7 @@
 (global-set-key (kbd "<f12>") 'comint-get-next-from-history)
 (autoload 'ansi-color-for-comint-mode-on "ansi-color" nil t)
 (add-hook 'shell-mode-hook 'ansi-color-for-comint-mode-on)
+(setenv "NODE_NO_READLINE" "1")
 (eshell)
 
 ;; open in current window
