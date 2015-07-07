74db50e94e787f7bf8c494d30f202e23c37eba0a
Tim Licata <tim@timothylicata.com>
2015-07-07 13:46:16 -0700

    slim mode for some ruby projects

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -58,6 +58,7 @@
                       paredit
                       pbcopy
                       projectile
+                      slim-mode
                       smex
                       web-mode
                       yasnippet
